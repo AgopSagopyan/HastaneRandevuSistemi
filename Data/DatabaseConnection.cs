9c13ee9eeae70a2d50218e07fff7cec0851e3a76
AgopSagopyan <keremcemerturk@gmail.com>
2026-03-13 15:39:06 +0300

    Important Changes
    I added MVC folder structure to the project

---
+++ Data/DatabaseConnection.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace HastaneRandevuSistemi.Data
+{
+    public class DatabaseConnection
+    {
+        private readonly string _connectionString;
+
+        public DatabaseConnection()
+        {
+            _connectionString = "Server=localhost;Database=deneme;Uid=root;Pwd=kerem4567;";
+        }
+
+        public MySqlConnection GetConnnection()
+        {
+            var conn = new MySqlConnection(_connectionString);
+
+            try
+            {
+                conn.Open();
+                return conn;
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("Could not connect to database");
+            }
+        }
+    }
+}
---
+++ Views/Pages/GirisYap.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HastaneRandevuSistemi.Controllers;
+using HastaneRandevuSistemi.Data;
+using MySql.Data.MySqlClient;
+
+namespace HastaneRandevuSistemi
+{
+    public partial class GirisYap : UserControl
+    {
+        private readonly LoginController _loginController;
+
+        public GirisYap()
+        {
+            InitializeComponent();
+            _loginController = new LoginController();
+        }
+
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text;
+            string password = textBox2.Text;
+
+            if (_loginController.Login(username, password)) {
+                MessageBox.Show("Login Successful!");
+            }
+
+
+        }
+    }
+}
